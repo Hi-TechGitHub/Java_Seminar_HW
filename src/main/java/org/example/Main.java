001fabc66fab262cc6587fe1dfa204aedfcfc576
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2023-03-03 02:04:10 +0300

    all is ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,23 @@
 package org.example;
+//Задать одномерный массив и найти в нем минимальный и максимальный элементы
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 1, 3, 4, 7, 11, 28};
+        findMaxAndMin(array);
+
+    }
+
+    private static void findMaxAndMin(int[] array) {
+        int min = 10;
+        int max = 0;
+        for (int i : array) {
+            if(max < i) {
+                max = i;
+            }
+            if(min > i) {
+                min = i;
+            }
+        }
+        System.out.println("Максимальное число это: " + max + ", Минимальное число это: " + min);
     }
 }
